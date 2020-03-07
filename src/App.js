b065de89ae715bf2075b03ba998a1feec5d952fe
squarerobin <squarerobin@users.noreply.github.com>
2020-03-07 06:39:14 +0100

    step 3 done

--- src/App.js
+++ src/App.js
@@ -2,35 +2,41 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+// Context
+import {ProductContext} from './contexts/ProductContext'
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
-	const [products, setProducts] = useState(data);
+	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
 		// step 1
-		setProducts([...products, item])
+		setCart( [...cart, item])
 
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{products, addItem}}>
+			<div className="App">
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}
+				<Route exact path="/">
+	{/* delete the props: products and addItem after step 4 and delete this comment*/}
+					<Products products={products} addItem={addItem} />
+				</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+				<Route path="/cart">
+					<ShoppingCart cart={cart} />
+				</Route>
+			</div>
+		</ProductContext.Provider>
 	);
 }
 
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,4 +1,3 @@
 import {createContext} from 'react'
-//TODO: continue here (read step2 in README.md)
 
 export const ProductContext = createContext()
